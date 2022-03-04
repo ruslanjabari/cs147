3515312cf8f4e3b11607dd1287cebe0b9150f577
nickWalks <69692804+nickWalks@users.noreply.github.com>
2022-03-04 09:16:04 -0800

    stashing app.js before pull

--- App.js
+++ App.js
@@ -162,12 +162,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   headerRightSearchSend: {
     display: 'flex',
     flexDirection: 'row',
