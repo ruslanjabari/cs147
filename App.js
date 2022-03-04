9ba76ab7955c8a19c57b4744c13ef71dc11a7052
Ruslan <rjabari@stanford.edu>
2022-03-03 23:40:25 -0800

    icon import-export fix

--- App.js
+++ App.js
@@ -7,25 +7,22 @@ import ArtistsFeed from './screens/ArtistsFeed';
 import HomeFeed from './screens/HomeFeed';
 import ProfileScreen from './screens/ProfileScreen';
 import CommunitiesScreen from './screens/CommunitiesScreen';
-import ArtistNFTs from './screens/ArtistNFTs'
+import ArtistNFTs from './screens/ArtistNFTs';
 import NFTDetails from './screens/NFTDetails';
 import PurchaseScreen from './screens/PurchaseScreen';
-import SearchIcon from './assets/icons/SearchIconFilled.svg';
-import DMIcon from './assets/icons/DMIconFilled.svg';
 
-import HomeIconSelected from './assets/icons/HomeIconSelected.svg';
-import HomeIconUnselected from './assets/icons/HomeIconUnselected.svg';
-
-
-import ProfileIconUnselected from './assets/icons/ProfileIconUnselected.svg';
-import ProfileIconSelected from './assets/icons/ProfileIconSelected.svg';
-
-
-import SparklesIconSelected from './assets/icons/SparklesIconSelected.svg';
-import SparklesIconUnselected from './assets/icons/SparklesIconUnselected.svg';
-
-import ChatIconSelected from './assets/icons/ChatIconSelected.svg';
-import ChatIconUnselected from './assets/icons/ChatIconUnselected.svg';
+import {
+  SearchIcon,
+  DMIcon,
+  HomeIconSelected,
+  HomeIconUnselected,
+  ProfileIconUnselected,
+  ProfileIconSelected,
+  SparklesIconSelected,
+  SparklesIconUnselected,
+  ChatIconSelected,
+  ChatIconUnselected,
+} from './assets/icons';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -36,22 +33,20 @@ import {
   Dosis_500Medium,
   Dosis_600SemiBold,
   Dosis_700Bold,
-  Dosis_800ExtraBold
-} from '@expo-google-fonts/dosis'
+  Dosis_800ExtraBold,
+} from '@expo-google-fonts/dosis';
 
-import {
-  Righteous_400Regular
-} from '@expo-google-fonts/righteous'
-
+import { Righteous_400Regular } from '@expo-google-fonts/righteous';
 
 const screenOptionsHeader = {
-  headerLeft: () => (<Text style={styles.teamName}>entourage</Text>), headerRight: () => (
+  headerLeft: () => <Text style={styles.teamName}>entourage</Text>,
+  headerRight: () => (
     <View style={styles.headerRightSearchSend}>
       <SearchIcon style={styles.searchIcon} />
       <DMIcon />
     </View>
   ),
-}
+};
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -62,7 +57,7 @@ export default function App() {
     Dosis_600SemiBold,
     Dosis_700Bold,
     Dosis_800ExtraBold,
-    Righteous_400Regular
+    Righteous_400Regular,
   });
   if (!fontsLoaded) return <AppLoading />;
 
@@ -72,37 +67,49 @@ export default function App() {
     const HomeStack = createStackNavigator();
 
     return (
-      <HomeStack.Navigator initialRouteName="HomeFeed" screenOptions={{ title: null }} >
+      <HomeStack.Navigator initialRouteName="HomeFeed" screenOptions={{ title: null }}>
         <HomeStack.Screen name="HomeFeed" component={HomeFeed} options={screenOptionsHeader} />
       </HomeStack.Navigator>
-    )
+    );
   }
 
   function SparklesStack() {
     const SparklesStack = createStackNavigator();
     return (
-      <SparklesStack.Navigator initialRouteName="ArtistsFeed" screenOptions={{ title: null }} >
-        <SparklesStack.Screen name="ArtistsFeed" component={ArtistsFeed} options={screenOptionsHeader} />
+      <SparklesStack.Navigator initialRouteName="ArtistsFeed" screenOptions={{ title: null }}>
+        <SparklesStack.Screen
+          name="ArtistsFeed"
+          component={ArtistsFeed}
+          options={screenOptionsHeader}
+        />
       </SparklesStack.Navigator>
-    )
+    );
   }
 
   function ChatStack() {
     const ChatStack = createStackNavigator();
     return (
-      <ChatStack.Navigator initialRouteName="CommunitiesScreen" screenOptions={{ title: null }} >
-        <ChatStack.Screen name="CommunitiesScreen" component={CommunitiesScreen} options={screenOptionsHeader} />
+      <ChatStack.Navigator initialRouteName="CommunitiesScreen" screenOptions={{ title: null }}>
+        <ChatStack.Screen
+          name="CommunitiesScreen"
+          component={CommunitiesScreen}
+          options={screenOptionsHeader}
+        />
       </ChatStack.Navigator>
-    )
+    );
   }
 
   function ProfileStack() {
     const ProfileStack = createStackNavigator();
     return (
-      <ProfileStack.Navigator initialRouteName="ProfileScreen" screenOptions={{ title: null }} >
-        <ProfileStack.Screen name="ProfileScreen" component={ProfileScreen} options={screenOptionsHeader} />
+      <ProfileStack.Navigator initialRouteName="ProfileScreen" screenOptions={{ title: null }}>
+        <ProfileStack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+          options={screenOptionsHeader}
+        />
       </ProfileStack.Navigator>
-    )
+    );
   }
 
   return (
@@ -115,9 +122,7 @@ export default function App() {
             let icon;
 
             if (route.name === 'Home') {
-              icon = focused
-                ? <HomeIconSelected />
-                : <HomeIconUnselected />;
+              icon = focused ? <HomeIconSelected /> : <HomeIconUnselected />;
             } else if (route.name === 'Artists') {
               icon = focused ? <SparklesIconSelected /> : <SparklesIconUnselected />;
             } else if (route.name === 'Chat') {
@@ -126,38 +131,52 @@ export default function App() {
               icon = focused ? <ProfileIconSelected /> : <ProfileIconUnselected />;
             }
 
-            return icon
+            return icon;
           },
           tabBarStyle: {
             paddingTop: 10,
             backgroundColor: 'black',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
           },
+        })}>
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeStack} options={{
-          headerShown: false
-        }} />
+        <Tab.Screen
+          name="Artists"
+          component={SparklesStack}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-        <Tab.Screen name="Artists" component={SparklesStack} options={{
-          headerShown: false
-        }} />
+        <Tab.Screen
+          name="Chat"
+          component={ChatStack}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-        <Tab.Screen name="Chat" component={ChatStack} options={{
-          headerShown: false
-        }} />
-
-        <Tab.Screen name="Profile" component={ProfileStack} options={{
-          headerShown: false
-        }} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileStack}
+          options={{
+            headerShown: false,
+          }}
+        />
         {/* <Stack.Screen name="ArtistNFTs" component={ArtistNFTs} /> */}
         {/* <Stack.Screen name="NFTDetails" component={NFTDetails} /> */}
         {/* <Stack.Screen name="PurchaseScreen" component={PurchaseScreen} /> */}
       </Tab.Navigator>
-    </NavigationContainer >
+    </NavigationContainer>
   );
 }
 
@@ -171,18 +190,18 @@ const styles = StyleSheet.create({
   headerRightSearchSend: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: "center",
+    justifyContent: 'center',
     flex: 1,
-    marginRight: 10
+    marginRight: 10,
   },
   teamName: {
     color: 'black',
     marginLeft: 10,
     fontSize: 20,
-    fontFamily: 'Righteous_400Regular'
+    fontFamily: 'Righteous_400Regular',
   },
   searchIcon: {
     marginRight: 10,
-    marginTop: 5
-  }
+    marginTop: 5,
+  },
 });
---
+++ assets/icons/index.js
@@ -0,0 +1,23 @@
+import SearchIcon from './SearchIconFilled.svg';
+import DMIcon from './DMIconFilled.svg';
+import HomeIconSelected from './HomeIconSelected.svg';
+import HomeIconUnselected from './HomeIconUnselected.svg';
+import ProfileIconUnselected from './ProfileIconUnselected.svg';
+import ProfileIconSelected from './ProfileIconSelected.svg';
+import SparklesIconSelected from './SparklesIconSelected.svg';
+import SparklesIconUnselected from './SparklesIconUnselected.svg';
+import ChatIconSelected from './ChatIconSelected.svg';
+import ChatIconUnselected from './ChatIconUnselected.svg';
+
+export {
+  SearchIcon,
+  DMIcon,
+  HomeIconSelected,
+  HomeIconUnselected,
+  ProfileIconUnselected,
+  ProfileIconSelected,
+  SparklesIconSelected,
+  SparklesIconUnselected,
+  ChatIconSelected,
+  ChatIconUnselected,
+};
